5ed0288d78674951661302fd9ad251c1464000e3
Luka-dot <79lukask@gmail.com>
2020-09-16 15:49:58 -0400

    string operators test

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 
+myString = 'this is my String For Testing'
+
+print(myString.upper())
+print(myString.find('i'))
+print('My' in myString)
+myString = myString.replace('m', 'M')
+print('My' in myString)
+# print(myString.replace('m', 'M'))
+print(myString)
+
+
+"""
 myList = [1,2,3,4,5,6,7]
 count = 0
 for num in myList:
     count = count + num
 
 print(count)
+"""

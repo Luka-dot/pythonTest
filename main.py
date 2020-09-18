957abf75e35345c2a0297ac8693c66b8f8f39866
Luka-dot <79lukask@gmail.com>
2020-09-18 09:00:47 -0400

    tupples

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
+numbers = (1, 2, 2, 2, 3, 4, 5, 6.6, 6.9)  #  () is tupple. imutable!!!
+
+numbers.count(2)    #returns 3 = total number of 2s in tupple
+print(numbers.index(6.6))   # index on value 6.6
+
+
+"""
+
 numbers = range(5, 10, 2)   # first argument is start value, 2nd arg is endin value, 3rd arg is step (by 2)
 for number in numbers:
     print(number)
@@ -7,7 +15,7 @@ for number in range(5):
     print(number)
 
 
-""""
+"
 
 numbers = [1, 2, 3, 4, 5, 6.6, 6.9]
 for item in numbers:

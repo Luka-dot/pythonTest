52a88cf3fc10d06a09a2a99335a539b0e99398d7
Luka-dot <79lukask@gmail.com>
2020-09-22 21:08:01 -0400

    function basic in Python

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def checkDriverAge(age=0):
 
 checkDriverAge(18)
 
+#   *args, **kwargs-> key words arguments
+
+def super_func(*args, **kwargs):
+    print(args)
+    return sum(args)
+
+print(super_func(1,2,3,4,5))
+
+def super_func2(*args, **kwargs):
+    total = 0
+    for items in kwargs.values():
+        total += items
+    return sum(args) + total
+
+print(super_func2(1,2,3,4,5, num1=5, num2=10))
+
 """
 
 def say_hello(name='Darth Vader', emoji='\U0001f600'):

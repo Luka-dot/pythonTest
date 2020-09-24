e34a9e194c3fc13ef303e1d4cddbaadaaab23d05
Luka-dot <79lukask@gmail.com>
2020-09-24 08:59:33 -0400

    Class Object attributes excer

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+#Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Fluffy', 4)
+cat2 = Cat('Bella', 3)
+cat3 = Cat('Moon', 6)
+
+
+# 2 Create a function that finds the oldest cat
+def get_oldest_cat(*args):
+    return max(args)
+
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f"The oldest cat is {get_oldest_cat(cat1.age, cat2.age, cat3.age)} years old.")
+
+
+
+"""
+
 class PlayerCharacter:
     # class object attribute
     membership = True
@@ -29,11 +55,6 @@ print(player2.name)
 player1.shout()
 # print(player1.magic_eight())
 # print(player2.specialAbility())
-
-
-
-
-"""
 
 def highest_even_number(list):
     highest_number = 0

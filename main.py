b63832324f05803cef344370682cf775092b73a0
Luka-dot <79lukask@gmail.com>
2020-09-25 15:55:50 -0400

    Class Object Super()

--- main.py
+++ main.py
@@ -1,4 +1,76 @@
 class User():
+    def __init__(self, email):
+        self.email = email
+
+    def sign_in(self):
+        print('logged in')
+
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        super().__init__(email)   #connects to supercalss
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'Attacking with power of {self.power}')
+
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f'Attacking with arrows: arrows left {self.num_arrows}')
+
+wizard1 = Wizard('Moje', 44, 'moje@gm.com')
+print(wizard1.email)
+
+"""
+
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#1 Add nother Cat
+class Suzy(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = [Simon('Simon', 4), Sally('Sally', 21), Suzy('Suzy', 1)]
+
+#3 Instantiate the Pet class with all your cats
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
+
+class User():
     def sign_in(self):
         print('logged in')
 
@@ -32,8 +104,6 @@ def player_attack(char):
 
 player_attack(wizard1)
 
-
-"""
 
 class PlayerCharacter:
     # class object attribute

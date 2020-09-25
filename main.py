924ff7490f26934ca829bf9c94b7c7394419ddaa
Luka-dot <79lukask@gmail.com>
2020-09-25 16:15:19 -0400

    Class Object Super()

--- main.py
+++ main.py
@@ -4,7 +4,37 @@ class User():
 
     def sign_in(self):
         print('logged in')
+class Toy():
+  def __init__(self, color, age):
+    self.color = color
+    self.age = age
+    self.my_dict = {
+        'name':'Yoyo',
+        'has_pets': False,
+    }
 
+  def __str__(self):
+    return f"{self.color}"
+
+  def __len__(self):
+    return 5
+
+  def __del__(self):
+    return "deleted"
+
+  def __call__(self):
+      return('yes??')
+
+  def __getitem__(self,i):
+      return self.my_dict[i]
+
+
+action_figure = Toy('red', 0)
+print(action_figure.__str__())
+print(str(action_figure))
+print(len(action_figure))
+print(action_figure())
+print(action_figure['name'])
 
 class Wizard(User):
     def __init__(self, name, power, email):

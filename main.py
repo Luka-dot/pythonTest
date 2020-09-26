54a3c530feac30aea869f2fd2be520404def0b38
Luka-dot <79lukask@gmail.com>
2020-09-26 08:23:06 -0400

    Class Object Super() excer

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+class SuperList(list):
+  def __len__(self):
+    return 1000
+
+super_list1 = SuperList();
+
+print(len(super_list1))
+super_list1.append(5)
+print(super_list1[0])
+print(issubclass(list, object))
+
 class User():
     def __init__(self, email):
         self.email = email

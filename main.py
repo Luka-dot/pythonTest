695da9875caa4a9320bbae9f503b5e37c7d8d572
Luka-dot <79lukask@gmail.com>
2020-09-28 11:57:26 -0400

    zip() in Py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def multiply_by2(item):
 print(list(map(multiply_by2, [1,2,3])))
 
 my_list = [1, 2, 3, 4, 5, 6, 7]
+your_list = [10, 11, 12, 13]
 
 def only_odd(item):
     return item % 2 != 0
 
 print(list(filter(only_odd, my_list)))
+print(list(zip(my_list, your_list)))

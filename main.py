987209ffbca39da07f14fa832597a5465ba20cea
Luka-dot <79lukask@gmail.com>
2020-09-28 11:51:51 -0400

    filter() in Py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ def multiply_by2(item):
     return item * 2
 
 print(list(map(multiply_by2, [1,2,3])))
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+def only_odd(item):
+    return item % 2 != 0
+
+print(list(filter(only_odd, my_list)))

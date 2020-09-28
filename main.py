13774bb730694e69d06e5e9aaf860909c803c706
Luka-dot <79lukask@gmail.com>
2020-09-28 18:41:53 -0400

    lambda expressions excers

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from functools import reduce
+
 my_list = [1,2,3,4,5]
 
 print(list(map(lambda item: item*2, my_list)))
 
+print(reduce(lambda acc, item: acc+item, my_list))
 
+another_list = [5,4,3]
+
+print(list(map(lambda  item: item*item, another_list)))
+
+# list sorting by 2nd item
+a = [(0,2), (4,3), (9,9), (10,-1)]
+a.sort(key=lambda number: number[1])
+print(a)
 
 
 '''

76cf3975813cc7f1c5522feb635df9ea9d050716
Luka-dot <79lukask@gmail.com>
2020-09-30 09:49:43 -0400

    dictonary comprehention excersize

--- main.py
+++ main.py
@@ -31,6 +31,20 @@ my_t = {num:num*2 for num in [1,2,3]}
 print(my_t)
 
 
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
+
+
+duplicates2= [x for x in some_list if some_list.count(x)]
+print(f'duplicates2 :', duplicates2)
+
 '''
 
 
